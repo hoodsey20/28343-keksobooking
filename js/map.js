d213107b53994301d7cbfcefdcf2ffae4381fea6
Артём <hoodsey20@gmail.com>
2018-02-17 14:08:18 +0700

    оформил карту в отдельный модуль

--- js/map.js
+++ js/map.js
@@ -1,30 +1,32 @@
 'use strict';
 
-var generatedOffers = window.data.generateOffers(window.consts.OFFER_QUANTITY);
-var mainPinElement = document.querySelector('.map__pin--main');
+(function () {
+  var generatedOffers = window.data.generateOffers(window.consts.OFFER_QUANTITY);
+  var mainPinElement = document.querySelector('.map__pin--main');
 
-var setAddress = function (xCoordinate, yCoordinate) {
-  var adressInputElement = document.querySelector('#address');
-  adressInputElement.value = Math.round(xCoordinate) + ', ' + Math.round(yCoordinate);
-};
+  var setAddress = function (xCoordinate, yCoordinate) {
+    var adressInputElement = document.querySelector('#address');
+    adressInputElement.value = Math.round(xCoordinate) + ', ' + Math.round(yCoordinate);
+  };
 
-var mainPinMouseupHandler = function (evt) {
-  var mainPinOffsetLeft = evt.currentTarget.offsetLeft;
-  var mainPinOffsetTop = evt.currentTarget.offsetTop;
-  var mainPinXCoordinate = mainPinOffsetLeft + window.consts.MAIN_PIN_WIDTH / 2;
-  var mainPinYCoordinate = mainPinOffsetTop + window.consts.MAIN_PIN_HEIGHT + window.consts.MAIN_PIN_PEAK_HEIGHT;
+  var mainPinMouseupHandler = function (evt) {
+    var mainPinOffsetLeft = evt.currentTarget.offsetLeft;
+    var mainPinOffsetTop = evt.currentTarget.offsetTop;
+    var mainPinXCoordinate = mainPinOffsetLeft + window.consts.MAIN_PIN_WIDTH / 2;
+    var mainPinYCoordinate = mainPinOffsetTop + window.consts.MAIN_PIN_HEIGHT + window.consts.MAIN_PIN_PEAK_HEIGHT;
 
-  setAddress(mainPinXCoordinate, mainPinYCoordinate);
-};
+    setAddress(mainPinXCoordinate, mainPinYCoordinate);
+  };
 
-var addMapPinsHandler = function () {
-  if (document.querySelectorAll('.map__pin').length < 2) {
-    window.renderMapPins(generatedOffers);
-  }
-};
+  var addMapPinsHandler = function () {
+    if (document.querySelectorAll('.map__pin').length < 2) {
+      window.renderMapPins(generatedOffers);
+    }
+  };
 
-window.appState.unsetActive();
+  window.appState.unsetActive();
 
-mainPinElement.addEventListener('mouseup', window.appState.setActive);
-mainPinElement.addEventListener('mouseup', addMapPinsHandler);
-mainPinElement.addEventListener('mouseup', mainPinMouseupHandler);
+  mainPinElement.addEventListener('mouseup', window.appState.setActive);
+  mainPinElement.addEventListener('mouseup', addMapPinsHandler);
+  mainPinElement.addEventListener('mouseup', mainPinMouseupHandler);
+})();
