9f3836df3c5290332c8a5f8c2974c7e4ad7128f1
Артём <hoodsey20@gmail.com>
2018-02-27 00:59:04 +0700

    переписал логику хэндлеров для формы фильтра

--- js/filters.js
+++ js/filters.js
@@ -1,23 +1,32 @@
 'use strict';
 
 (function () {
-  var filterSelectElements = document.querySelectorAll('.map__filter');
-  var filterFeaturesElements = document.querySelectorAll('.features input');
+  var filterFormElements = document.querySelector('.map__filters').elements;
 
-  for (var i = 0; i < filterSelectElements.length; i++) {
-    filterSelectElements[i].addEventListener('change', function (evt) {
-      window.filters[evt.target.name] = evt.target.value;
-    });
-  }
+  var checkboxChangeHandler = function (evt) {
+    if (evt.target.checked) {
+      window.filters.features.push(evt.target.value);
+    } else {
+      window.filters.features = window.filters.features.filter(function (item) {
+        return item !== evt.target.value;
+      });
+    }
+  };
 
-  for (var j = 0; j < filterFeaturesElements.length; j++) {
-    filterFeaturesElements[j].addEventListener('change', function (evt) {
-      window.filters.features[evt.target.value] = evt.target.checked;
-    });
+  var selectChangeHandler = function (evt) {
+    window.filters[evt.target.name] = evt.target.value;
+  };
+
+  for (var i = 0; i < filterFormElements.length; i++) {
+    if (filterFormElements[i].type === 'checkbox') {
+      filterFormElements[i].addEventListener('change', checkboxChangeHandler);
+    } else if (filterFormElements[i].tagName === 'SELECT') {
+      filterFormElements[i].addEventListener('change', selectChangeHandler);
+    }
   }
 
   window.filters = {
-    'features': {},
+    'features': [],
   };
 
 })();
