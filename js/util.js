e5722b7dfe788ab86b47146553548e813e9f8e67
Артём <hoodsey20@gmail.com>
2018-03-02 20:31:39 +0700

    удалил неиспользуемые функции

--- js/util.js
+++ js/util.js
@@ -3,14 +3,6 @@
 (function () {
   window.util = {};
 
-  window.util.getRandomInInterval = function (min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  };
-
-  window.util.compareWithRandomResult = function () {
-    return Math.random() - 0.5;
-  };
-
   window.util.getRightWorldForm = function (num, one, two, five) {
     var number = Math.abs(num);
     var remainderOfDivisionBy100 = number % 100;
