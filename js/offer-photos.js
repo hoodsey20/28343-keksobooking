ae6d9d03b5ef39a1ac032cf6961ab7540df1ea24
Артём <hoodsey20@gmail.com>
2018-03-05 21:03:21 +0700

    превью для подгружаемых файлов

---
+++ js/offer-photos.js
@@ -0,0 +1,44 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var photoContainerElement = document.querySelector('.form__photo-container');
+  var photoInputElement = photoContainerElement.querySelector('input[type="file"]');
+
+  var clearPreviews = function () {
+    var previewElements = photoContainerElement.querySelectorAll('.form__photo');
+    if (previewElements) {
+      previewElements.forEach(function (previewElement) {
+        previewElement.remove();
+      });
+    }
+  };
+
+  var createPreview = function (file) {
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+
+      reader.addEventListener('load', function () {
+        var previewElement = document.createElement('div');
+        previewElement.classList.add('form__photo');
+        previewElement.style.backgroundImage = 'url(' + reader.result + ')';
+        photoContainerElement.appendChild(previewElement);
+      });
+    }
+  };
+
+  photoInputElement.addEventListener('change', function () {
+    clearPreviews();
+    var uploadedfiles = photoInputElement.files;
+    Object.keys(uploadedfiles).forEach(function (file) {
+      createPreview(uploadedfiles[file]);
+    });
+  });
+})();
